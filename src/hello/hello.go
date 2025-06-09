7e26294854dd3c194a7bd72954592a1e058ac799
erimendes <eri.mendes32@gmail.com>
2025-06-09 16:37:18 -0300

    Comando Scan e Scanf

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -12,4 +12,14 @@ func main() {
 	fmt.Println("Este programa está na versão: ", versao)
 
 	fmt.Println("O tipo da variável versão é: ", reflect.TypeOf(versao))
+
+	fmt.Println("1- Iniciar Monitoramento")
+	fmt.Println("2- Exibir Logs")
+	fmt.Println("0- Sair do Programa")
+
+	var comando int
+	// fmt.Scanf("%d", &comando)
+	fmt.Scan(&comando)
+	fmt.Println("O endereço da minha variável comando é: ", &comando)
+	fmt.Println("O comando escolhido foi: ", comando)
 }
